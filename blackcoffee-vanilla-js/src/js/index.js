339f331a97fb8931773c3ac7b9dfeb41ec326726
garudanish <hyeonsu.kim.dev@gmail.com>
2022-02-25 18:17:07 +0900

    메뉴 삭제하기

--- blackcoffee-vanilla-js/src/js/index.js
+++ blackcoffee-vanilla-js/src/js/index.js
@@ -7,12 +7,22 @@
 // - [x] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
 // - [x] 사용자 입력값이 빈 값이라면 추가되지 않는다.
 
+// TODO 메뉴 수정
+// - [x] 메뉴의 수정 버튼 클릭 이벤트를 받고, 메뉴 이름을 수정하는 모달창이 뜬다.
+// - [x] 모달 창에서 신규 메뉴 명을 입력받고, 확인 버튼을 누르면 메뉴가 수정된다.
+
 const $ = (selector) => document.querySelector(selector);
 
 function App() {
-  // TODO 메뉴 수정
-  // - [x] 메뉴의 수정 버튼 클릭 이벤트를 받고, 메뉴 이름을 수정하는 모달창이 뜬다.
-  // - [x] 모달 창에서 신규 메뉴 명을 입력받고, 확인 버튼을 누르면 메뉴가 수정된다.
+  // TODO 메뉴 삭제
+  // - [ ] 메뉴 삭제 버튼 클릭 이벤트를 받고, 메뉴 삭제 confirm 모달창이 뜬다.
+  // - [ ] 확인 버튼을 클릭하면 메뉴가 삭제 된다.
+  // - [ ] 총 메뉴 갯수를 count하여 상단에 보여준다.
+
+  const updateMenuCount = () => {
+    const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
+    $(".menu-count").innerText = `총 ${menuCount}개`;
+  };
 
   $("#espresso-menu-list").addEventListener("click", (e) => {
     if (e.target.classList.contains("menu-edit-button")) {
@@ -22,6 +32,13 @@ function App() {
         $menuName.innerText
       );
       $menuName.innerText = updatedMenuName;
+    }
+
+    if (e.target.classList.contains("menu-remove-button")) {
+      if (confirm("정말 삭제하시겠습니까?")) {
+        e.target.closest("li").remove();
+        updateMenuCount();
+      }
     }
   });
 
@@ -57,8 +74,7 @@ function App() {
       "beforeend",
       menuItemTemplate(espressoMenuName)
     );
-    const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
-    $(".menu-count").innerText = `총 ${menuCount}개`;
+    updateMenuCount();
     $("#espresso-menu-name").value = "";
   };
 
@@ -75,8 +91,3 @@ function App() {
 }
 
 App();
-
-// TODO 메뉴 삭제
-// - [ ] 메뉴 삭제 버튼 클릭 이벤트를 받고, 메뉴 삭제 confirm 모달창이 뜬다.
-// - [ ] 확인 버튼을 클릭하면 메뉴가 삭제 된다.
-// - [ ] 총 메뉴 갯수를 count하여 상단에 보여준다.

7066f8c3ce5818cf2022b4c7beb2efb07e48125c
garudanish <hyeonsu.kim.dev@gmail.com>
2022-02-28 11:01:15 +0900

    크롬 확장 앱 만들기: popup html 구현

--- K-Digital-Credit/만들면서 복습하는 JavaScript/chrome-extension/typdUrls.js
+++ K-Digital-Credit/만들면서 복습하는 JavaScript/chrome-extension/typdUrls.js
@@ -1,7 +1,23 @@
+// 클릭 시 새 탭에서 열기
 function onAnchorClick(event) {
   chrome.tabs.create({
     selected: true,
     url: event.srcElement.href,
   });
   return false;
+}
+
+// Top 10 Visited url 배열이 주어졌을 때
+// 각각의 url들을 typedUrls.html에 띄워주기
+function buildPopupDom(divName, data) {
+  var popDiv = document.getElementById(divName);
+
+  var ul = document.createElement("ul");
+  popDiv.appendChild(ul);
+
+  for (var i = 0; i < data.length; i++) {
+    var a = document.createElement("a");
+    a.href = data[i];
+    a.appendChild(document.createTextNode(data[i]));
+  }
 }

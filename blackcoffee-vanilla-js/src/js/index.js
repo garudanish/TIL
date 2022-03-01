d58d5ecd6444e994b0e9b917e9a042413b263ff7
garudanish <hyeonsu.kim.dev@gmail.com>
2022-03-01 11:26:08 +0900

    Step 3 - 메뉴 데이터 초기화하기

--- blackcoffee-vanilla-js/src/js/index.js
+++ blackcoffee-vanilla-js/src/js/index.js
@@ -16,6 +16,13 @@
 
 const BASE_URL = "http://localhost:3000/api";
 
+const MenuApi = {
+  async getAllMenuByCategory(category) {
+    const response = await fetch(`${BASE_URL}/category/${category}/menu`);
+    return response.json();
+  },
+};
+
 import { $ } from "./utils/dom.js";
 import store from "./store/index.js";
 
@@ -29,10 +36,10 @@ function App() {
     desert: [],
   };
   this.currentCategory = "espresso";
-  this.init = () => {
-    if (store.getLocalStorage()) {
-      this.menu = store.getLocalStorage();
-    }
+  this.init = async () => {
+    this.menu[this.currentCategory] = await MenuApi.getAllMenuByCategory(
+      this.currentCategory
+    );
     render();
     initEventListeners();
   };
@@ -93,15 +100,11 @@ function App() {
       return response.json();
     });
 
-    await fetch(`${BASE_URL}/category/${this.currentCategory}/menu`)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        this.menu[this.currentCategory] = data;
-        render();
-        $("#menu-name").value = "";
-      });
+    this.menu[this.currentCategory] = await MenuApi.getAllMenuByCategory(
+      this.currentCategory
+    );
+    render();
+    $("#menu-name").value = "";
   };
 
   const updateMenuName = (e) => {

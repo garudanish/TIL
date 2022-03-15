dc3c5cc1be1b1905804f717d3ddd8c37e6f4110a
garudanish <hyeonsu.kim.dev@gmail.com>
2022-03-16 05:25:10 +0900

    Bcrypt로 비밀번호 암호화하기

--- john-ahn-lectures/01-boiler-plate/models/User.js
+++ john-ahn-lectures/01-boiler-plate/models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 const userSchema = mongoose.Schema({
   name: {
     type: String,
@@ -28,6 +31,28 @@ const userSchema = mongoose.Schema({
   tokenExp: {
     type: Number,
   },
+});
+
+userSchema.pre("save", function (next) {
+  var user = this;
+
+  if (user.isModified("password")) {
+    // 비밀번호를 암호화시킨다.
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) {
+        return next(err);
+      }
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) {
+          return next(err);
+        }
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
 });
 
 const User = mongoose.model("User", userSchema);

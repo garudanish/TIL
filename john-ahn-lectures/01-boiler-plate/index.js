3215f0e3dc1521c003e5b77109ceba74444436d7
garudanish <hyeonsu.kim.dev@gmail.com>
2022-03-15 20:24:08 +0900

    비밀 정보 보호

---
+++ john-ahn-lectures/01-boiler-plate/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const bodyParser = require("body-parser");
+const config = require("./config/key");
+const { User } = require("./models/User");
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// application/json
+app.use(bodyParser.json());
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(config.mongoURI)
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello World! 안녕하세요! Hello World!");
+});
+
+app.post("/register", (req, res) => {
+  // 회원가입할 때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터베이스에 넣어준다.
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});

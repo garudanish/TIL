a2407748c3f53b2dddff8b25928fba62350f9ad2
Hyeonsu Kim <hyeonsu.kim.dev@gmail.com>
2022-06-06 16:41:10 +0900

    NextJS: Assets, Metadata, and CSS

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,16 +1,20 @@
-import React from "react";
+import Head from "next/head";
 import Link from "next/link";
+import Layout from "../../components/layout";
 
 const FistPost = () => {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">
           <a>Back to Home</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   );
 };
 
